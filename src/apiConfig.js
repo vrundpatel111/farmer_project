ac4680568497939c8b08760984afcddddea8f63f
vrund patel <vrundp1811@gmail.com>
2026-04-30 22:17:10 +0530

    Configure Vercel and PostgreSQL for deployment

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,3 @@
-import API_BASE_URL from "apiConfig";
-const API_BASE_URL = process.env.REACT_APP_API_URL || `${API_BASE_URL}`;
+const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
 
 export default API_BASE_URL;
